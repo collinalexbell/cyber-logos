46803f21531d449a257fa5aa7db98812ae2b9d5c
kuberlog <collinalexbell@gmail.com>
2023-03-04 20:59:07 -0500

    factory-iteration compiles

--- factory.lisp
+++ factory.lisp
@@ -1,10 +1,12 @@
 (in-package :logos)
 (defun work-on-cyber-logos ())
 (defun work-on-kuberdog ())
+(defun take-a-break ())
+(defmacro do-work-until-tired (&rest body) `(progn ,@body))
 
 
 (defun factory-iteration ()
-  (do-until-until-tired
+  (do-work-until-tired
    (work-on-cyber-logos)
    (work-on-kuberdog))
-  (break))
+  (take-a-break))
