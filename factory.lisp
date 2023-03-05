61abc55854857e2dfaa0ca4709ab7d17127514b4
kuberlog <collinalexbell@gmail.com>
2023-03-04 20:54:46 -0500

    add factory.lisp

---
+++ factory.lisp
@@ -0,0 +1,10 @@
+(in-package :logos)
+(defun work-on-cyber-logos ())
+(defun work-on-kuberdog ())
+
+
+(defun factory-iteration ()
+  (do-until-until-tired
+   (work-on-cyber-logos)
+   (work-on-kuberdog))
+  (break))
