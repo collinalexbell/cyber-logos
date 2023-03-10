ac741af864800691a336f94e2448c2bb72f355d7
kuberlog <collinalexbell@gmail.com>
2023-03-10 04:28:04 -0500

    connection to postgres works

--- goal/repo.lisp
+++ goal/repo.lisp
@@ -5,25 +5,29 @@
 (load "./goal.lisp")
 
 (defpackage :logos.goal.repo
-  (:use :cl :postmodern :logos.goal))
+  (:use :cl :postmodern :logos.goal :cl-json :uiop))
 
 (in-package :logos.goal.repo)
 
-(defun config-dbname (config))
-(defun config-dbuser (config))
-(defun config-dbpass (config))
-(defun config-dburl (config))
+(defun config-dbname
+    (config)
+  (cdr (assoc :dbname (json:decode-json-from-string config))))
+
+(defun config-dbuser
+    (config)
+  (cdr (assoc :dbuser (json:decode-json-from-string config))))
+
+(defun config-dbpass
+    (config)
+  (cdr (assoc :dbpass (json:decode-json-from-string config))))
+
+(defun config-dburl
+    (config)
+  (cdr (assoc :dburl (json:decode-json-from-string config))))
 
 (defun connect-with-config ()
-    (with-open-file (
-
-                  config
-                  "config"
-                  :direction :input
-
-                  )
-
-   (let ((dbname
+  (let* ((config (apply #'concatenate 'string (uiop:read-file-lines "./goal/config.json")))
+         (dbname
            (config-dbname config))
          (user
            (config-dbuser config))
@@ -33,10 +37,10 @@
            (config-dburl config)))
 
 
-     (connect-toplevel dbname
-                       user
-                       pass
-                       url))))
+    (connect-toplevel dbname
+                      user
+                      pass
+                      url)))
 
 
 (defun read-goal-from-json (json-string)
@@ -47,3 +51,12 @@
                    :name (cdr (assoc 'name json-object))
                    :deadline (cdr (assoc 'deadline json-object))
                    :notes (cdr (assoc 'notes json-object)))))
+
+
+(defun write-goal-to-postgres (goal)
+  (let ((id (slot-value goal 'logos.goal:id))
+        (name (slot-value goal 'logos.goal:name))
+        (deadline (slot-value goal 'logos.goal:deadline))
+        (notes (slot-value goal 'logos.goal:notes)))
+    (postmodern:execute "INSERT INTO goals (id, name, deadline, notes) VALUES ($1, $2, $3, $4)"
+                        (list id name deadline notes))))
