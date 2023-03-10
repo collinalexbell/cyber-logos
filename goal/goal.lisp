556a3dea6507c4de84280466116000779f91fed9
kuberlog <collinalexbell@gmail.com>
2023-03-10 08:04:38 -0500

    goal.lisp

--- goal/goal.lisp
+++ goal/goal.lisp
@@ -6,3 +6,12 @@
    (name :initarg :name)
    (deadline :initarg :deadline)
    (notes :initarg :notes)))
+
+
+;;; Be Obsesed or Be Average: Ch4: 3:24:33 on audible
+;;;; Every order should come with a ... report
+(defun report (goal)
+  "Creates a report for the goal")
+
+(defun next-report-time (goal)
+  "Returns the time the next report should be generated")
