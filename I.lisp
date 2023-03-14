32ac324dd72a12f7aa151749d62e2bea097adbbd
kuberlog <collinalexbell@gmail.com>
2023-03-14 03:09:08 -0400

    use the I from holon

--- I.lisp
+++ I.lisp
@@ -1,3 +1,23 @@
+(in-package :logos)
 
-(defvar I '((virtue . '(Courage))))
 (defvar current-virtue-focus Courage)
+
+(defun init-I ()
+                                        ; create a new I if one can't be loaded
+  (if (not (boundp 'I))
+      (progn
+        (defvar I (new-human 'Collin))
+        (setf (slot-value I 'superior-logoss)
+              `(USA
+                NYC
+                ,@(slot-value I 'superior-logoss)))
+        (setf (slot-value I 'inferior-logoss)
+              `(mind
+                body
+                ,@(slot-value I 'inferior-logoss)))
+
+        (setf (slot-value I 'inferior-logoss) (cons logos.task:*selected-task* (inferior-logoss I)))))
+  I)
+
+(init-I)
+;(defvar I (restore "i.holon"))
