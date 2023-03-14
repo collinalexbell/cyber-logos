0a1cebe6cbd26d2f4b6da72400202b7e54e0ae20
kuberlog <collinalexbell@gmail.com>
2023-03-14 02:52:38 -0400

    add packages and classes

--- cyber-logos.lisp
+++ cyber-logos.lisp
@@ -3,4 +3,5 @@
 ;This will eventually be the main entry point to the executable
 ;For now, just show the capabilities
 (defun cyber-logos ()
-  '((classes (Logos Task Vitue))))
+  '((classes (Logos Task Vitue))
+    (packages (:logos :logos.task))))
