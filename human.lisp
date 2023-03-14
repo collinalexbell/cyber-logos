a745e3d1b98364e9d696f5f7729d645b71f37006
kuberlog <collinalexbell@gmail.com>
2023-03-14 06:22:43 -0400

    init human.lisp, almost exactly the same as the one in holon

---
+++ human.lisp
@@ -0,0 +1,29 @@
+(in-package :logos)
+(defclass human (logos)
+  (tasks
+   (profession
+    :initform '()
+    :initarg profesion
+    :accessor profession)
+   (positions
+    :initform '()
+    :initarg positions
+    :accessor positions)))
+
+(defmacro with-human (the-human &rest body)
+  ;; threads the human through firt argument of all forms in body
+  ;; body should like body of progn or defun
+  (cons 'progn
+     (loop for form in body collect (cons (car form) (cons the-human (cdr form))))))
+
+(defmethod renew ((person human))
+  (format t "Have a good rest human."))
+
+(defun new-human (name)
+  (make-instance 'human :name name))
+
+(defun save (human file)
+  (cl-store:store human file))
+
+(defun restore (file)
+  (cl-store:restore file))
