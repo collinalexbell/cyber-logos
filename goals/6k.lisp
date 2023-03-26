9e4ac51ef5a4a726164d2f705ae324cd53a086be
kuberlog <collinalexbell@gmail.com>
2023-03-25 21:51:55 -0400

    Eh, at least I got that scheduling and outstream surface area defined.

--- goals/6k.lisp
+++ goals/6k.lisp
@@ -2,10 +2,24 @@
 
 (in-package :logos.goals.6k)
 
+(defun runner-outstream ()
+  ; returns an outstream for the cyberlogos to attach its output to
+  )
+
+(defun schedule-with-outstream (when-to-run outstream)
+  (list when-to-run outstream)
+  ;; this is what creates the timer instances
+  )
+
 (defmacro run-a-cyberlogos (&rest args)
   (let ((when-to-run (car args)))
     `(progn
-       (format t "when-to-run: ~a~%" (quote ,when-to-run)))))
+       (format t "when-to-run: ~a~%" (quote ,when-to-run))
+
+       ;; a stream is needed, because these closures will be ran on a schedule
+       ;; std:out can not be reliably used
+       (schedule-with-outstream
+        (quote ,when-to-run) (runner-outstream)))))
 
 
 (defun test-run-a-cyberlogos ()
