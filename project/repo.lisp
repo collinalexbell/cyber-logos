6e1e3e3cefdde4201364fc234ed130ab55df76d0
kuberlog <collinalexbell@gmail.com>
2023-04-04 16:09:52 -0400

    init-db is gtg, all-from-prostgres still needs work

--- project/repo.lisp
+++ project/repo.lisp
@@ -1,4 +1,22 @@
-(ql:quickload :postmodern)
 (defpackage :logos.project.repo
   (:use :cl :postmodern))
 (in-package :logos.project.repo)
+
+(defun init-db ()
+  (handler-case
+   (postmodern:execute "CREATE TABLE project (
+id SERIAL PRIMARY KEY,
+name text)")
+    (cl-postgres-error:duplicate-table ()
+      (format t "duplicate table"))))
+
+(defun all-from-postgres ()
+  (defvar goals
+    (loop for goal-list
+            in (postmodern:query
+                "select * from goal")
+          collect
+          (interleave
+           '(:id :name :deadline :notes)
+           goal-list))))
+
