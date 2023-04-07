bb327d3cb776ea5737d2cc3f98fa0271d6b660fa
kuberlog <collinalexbell@gmail.com>
2023-04-06 22:33:29 -0400

    This guy is awesome

---
+++ people/alex-hermozi.lisp
@@ -0,0 +1,6 @@
+
+(defvar what-makes-an-entrepreneur-according-to-hermozi
+  '((influence)
+    (big motivator)
+    (can control themselves long enough to keep on going)
+    (know the inputs and outputs (outcome and how to get it))))
