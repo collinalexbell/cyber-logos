0892cf0cc708a237985548d5864b857dfa530da5
kuberlog <collinalexbell@gmail.com>
2023-04-08 19:13:35 -0400

    fix fomratting

--- db/postgres.lisp
+++ db/postgres.lisp
@@ -8,36 +8,24 @@
 
 (in-package :logos.db.postgres)
 
-(defun config-dbname
-    (config)
+(defun config-dbname (config)
   (cdr (assoc :dbname (json:decode-json-from-string config))))
 
-(defun config-dbuser
-    (config)
+(defun config-dbuser (config)
   (cdr (assoc :dbuser (json:decode-json-from-string config))))
 
-(defun config-dbpass
-    (config) (cdr (assoc :dbpass (json:decode-json-from-string config))))
+(defun config-dbpass (config)
+  (cdr (assoc :dbpass (json:decode-json-from-string config))))
 
-(defun config-dburl
-    (config)
+(defun config-dburl (config)
   (cdr (assoc :dburl (json:decode-json-from-string config))))
 
 (defun connect-with-config ()
   (let* ((config (apply #'concatenate 'string (uiop:read-file-lines "db/postgres_config.json")))
-         (dbname
-           (config-dbname config))
-         (user
-           (config-dbuser config))
-         (pass
-           (config-dbpass config))
-         (url
-           (config-dburl config)))
-
-
-    (connect-toplevel dbname
-                      user
-                      pass
-                      url)))
+         (dbname (config-dbname config))
+         (user (config-dbuser config))
+         (pass (config-dbpass config))
+         (url (config-dburl config)))
+    (connect-toplevel dbname user pass url)))
 
 
