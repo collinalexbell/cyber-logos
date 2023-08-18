c04e5210183f07abf8a6e2e401df03b383ce0015
kuberlog <collinalexbell@gmail.com>
2023-08-18 02:21:11 +0000

    function of a king

--- tradition/tradition.lisp
+++ tradition/tradition.lisp
@@ -30,3 +30,10 @@
                 autonomy))
     (name heroic-asceticism)
     (aphorism "A man is a mortal god, and a god is an immortal man")))
+
+
+(defun king ()
+  '((function (act as a bridge to the transcendent))
+    (imposition (power-through-spirit sporadic-physical-force))
+    (inferior-attributes (strength violence intelligence wisdom physical-courage concern-for-community))
+    ))
