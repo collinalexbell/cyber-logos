8bd4b738410172db1acd914762e18128040cd990
kuberlog <collinalexbell@gmail.com>
2023-08-30 09:57:56 +0000

    waves

--- phoenix/phoenix.lisp
+++ phoenix/phoenix.lisp
@@ -1,6 +1,8 @@
 (defpackage :logos.pheonix (:use :cl :logos.task))
 (in-package :logos.pheonix)
 
+(load "phoenix/social-media-moderation")
+
 (defun mission ()
   '(I spend a fully pre-funded year
     (studying planning designing and implementing
@@ -22,7 +24,7 @@
 
 (defvar routine-count 0)
 
-(defun routine ()
+(defun short-wave ()
   (add-tasks '((spiritual reading)
                (meditate)
                (technical reading)
@@ -31,49 +33,13 @@
   (format t "Routines ran: ~a~%" (incf routine-count))
   (format t "~%~%DO NOT GO ON TWITTER~%~%"))
 
-(defun wmctrl ()
-  (let ((output (make-string-output-stream)))
-    (sb-ext:run-program "/usr/bin/wmctrl"
-                        '("-l") :output output)
-    (get-output-stream-string output)))
+;; figure out a system to integrate these waves
+(defun medium-wave ()
+  (add-tasks '((walk dog)
+               (make coffee))))
 
-(defun window-open? (in-name)
-  (not (null
-        (search in-name (wmctrl)))))
-
-(defun refocus ()
-  (sb-ext:run-program "/usr/bin/mplayer"
-                      '("media/singing_bowl.mp4"
-                        "-xy" "400"
-                        "-pausing" "2")))
-
-(defun X-is-open? ()
-  (window-open? "/ X "))
-
-(defun reddit-is-open? ()
-  (window-open? "Reddit"))
-
-(defun focus-loop ()
-  (loop do (if (or (X-is-open?) (reddit-is-open?))
-               (refocus)
-               (sleep 1))))
-
-(defun start-social-media-moderation ()
-  (let ((focus-thread
-          (sb-thread:make-thread #'focus-loop)))
-    (defun stop-social-media-moderation ()
-      (sb-thread:terminate-thread focus-thread))))
-
-
-(defun status ()
-  (tasks)
-  (format t "~%~%DO NOT GO ON TWITTER~%~%"))
-
-(defun tweets-not-short-enough (tweets)
-  ;; use loop
-  (loop for tweet in tweets
-        when (> (length tweet) 280)
-          collect tweet))
-
+(defun long-wave ()
+  (add-tasks '((clean)
+               (shower))))
 
 
