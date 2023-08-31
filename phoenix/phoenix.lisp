c8bce95daa579222a172b56853287e3d51bd511c
kuberlog <collinalexbell@gmail.com>
2023-08-31 09:39:21 +0000

    fix redundancy

--- phoenix/phoenix.lisp
+++ phoenix/phoenix.lisp
@@ -46,29 +46,17 @@
                ((eq time :evening) '((shower))))))
 
 (defun start-time-of-day-cron ()
-  (let ((morning (sb-ext:make-timer (lambda () (time-of-day-tasks :morning))))
-        (afternoon (sb-ext:make-timer (lambda () (time-of-day-tasks :afternoon))))
-        (evening (sb-ext:make-timer (lambda () (time-of-day-tasks :evening))))
+  (let ((timer-params '((:morning 7) (:afternoon 12) (:evening 16)))
         (utc-offset 4))
-    (local-time:with-decoded-timestamp (:day day :month month :year year)
-                                       (local-time:today)
-      (sb-ext:schedule-timer morning (local-time:timestamp-to-universal
-                                      (local-time:encode-timestamp
-                                       0 0 0 (+ 7 utc-offset) day month year))
-                             :repeat-interval (* 24 60 60)
-                             :absolute-p t)
-
-      (sb-ext:schedule-timer afternoon (local-time:timestamp-to-universal
-                                      (local-time:encode-timestamp
-                                       0 0 0 (+ 12 utc-offset) day month year))
-                             :repeat-interval (* 24 60 60)
-                             :absolute-p t)
-
-      (sb-ext:schedule-timer evening (local-time:timestamp-to-universal
-                                        (local-time:encode-timestamp
-                                         0 0 0 (+ 4 utc-offset) day month year))
-                             :repeat-interval (* 24 60 60)
-                             :absolute-p t))))
+    (loop for timer-param in timer-params do
+      (local-time:with-decoded-timestamp (:day day :month month :year year)
+                                         (local-time:today)
+        (sb-ext:schedule-timer (sb-ext:make-timer (lambda () (time-of-day-tasks (car timer-param))))
+                               (local-time:timestamp-to-universal
+                                (local-time:encode-timestamp
+                                 0 0 0 (+ (cadr timer-param) utc-offset) day month year))
+                               :repeat-interval (* 24 60 60)
+                               :absolute-p t)))))
 
 
 
