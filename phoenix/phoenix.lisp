9f6622826dcac245a7a2c9672987a2d1afd0db57
kuberlog <collinalexbell@gmail.com>
2023-08-31 05:26:10 +0000

    time-of-day-tasks

--- phoenix/phoenix.lisp
+++ phoenix/phoenix.lisp
@@ -38,8 +38,37 @@
   (add-tasks '((walk dog)
                (make coffee))))
 
-(defun long-wave ()
-  (add-tasks '((clean)
-               (shower))))
+(defun time-of-day-tasks (time)
+  ;; run once per time of day
+  (add-tasks (cond
+               ((eq time :afternoon) '((run)))
+               ((eq time :morning) '((clean) (make-coffee)))
+               ((eq time :evening) '((shower))))))
+
+(defun start-time-of-day-cron ()
+  (let ((morning (sb-ext:make-timer (lambda () (time-of-day-tasks :morning))))
+        (afternoon (sb-ext:make-timer (lambda () (time-of-day-tasks :afternoon))))
+        (evening (sb-ext:make-timer (lambda () (time-of-day-tasks :evening))))
+        (utc-offset 4))
+    (local-time:with-decoded-timestamp (:day day :month month :year year)
+                                       (local-time:today)
+      (sb-ext:schedule-timer morning (local-time:timestamp-to-universal
+                                      (local-time:encode-timestamp
+                                       0 0 0 (+ 7 utc-offset) day month year))
+                             :repeat-interval (* 24 60 60)
+                             :absolute-p t)
+
+      (sb-ext:schedule-timer afternoon (local-time:timestamp-to-universal
+                                      (local-time:encode-timestamp
+                                       0 0 0 (+ 12 utc-offset) day month year))
+                             :repeat-interval (* 24 60 60)
+                             :absolute-p t)
+
+      (sb-ext:schedule-timer evening (local-time:timestamp-to-universal
+                                        (local-time:encode-timestamp
+                                         0 0 0 (+ 4 utc-offset) day month year))
+                             :repeat-interval (* 24 60 60)
+                             :absolute-p t))))
+
 
 
