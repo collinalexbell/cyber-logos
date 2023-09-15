1af0420adad2d9cdf23addb82acf0ce6f8e921c9
kuberlog <collinalexbell@gmail.com>
2023-09-15 10:40:25 +0000

    simple cube renderer

--- phoenix/matrix.lisp
+++ phoenix/matrix.lisp
@@ -2,11 +2,21 @@
 ;; it couldn't find the shared lib on Ubutu
 (push "/usr/local/lib/" cffi:*foreign-library-directories*)
 (ql:quickload :pzmq)
-(defpackage :logos.phoenix.matrix (:use :cl))
+(defpackage :logos.phoenix.matrix (:use :cl) (:export :add-cube :add-a-logos))
 (in-package :logos.phoenix.matrix)
 
 (defun cube-to-str (x y z)
   (format nil "c ~a ~a ~a" x y z))
+
+(defun add-a-logos (a-logos x y z)
+  (add-cube x y z)
+  (let ((inferiors (logos:inferior-logoss a-logos)))
+   (loop for logos in inferiors
+         for inferior-x from (* -3 (floor (length inferiors) 2)) to (* 3 (ceiling (length inferiors) 2)) :by 3
+         do
+            (progn
+              (prin1 `(add-cube ,inferior-x ,(- y 3) ,z))
+              (add-cube inferior-x (- y 3) z)))))
 
 (defun add-cube (x y z)
     (pzmq:with-socket socket :req
