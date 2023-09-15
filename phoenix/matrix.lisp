36071aa08118aa94ec5806a7a242ca108172480c
kuberlog <collinalexbell@gmail.com>
2023-09-15 08:14:33 +0000

    pzmq can create cubes in matrix!

--- phoenix/matrix.lisp
+++ phoenix/matrix.lisp
@@ -4,3 +4,11 @@
 (ql:quickload :pzmq)
 (defpackage :logos.phoenix.matrix (:use :cl))
 (in-package :logos.phoenix.matrix)
+
+(defun cube-to-str (x y z)
+  (format nil "c ~a ~a ~a" x y z))
+
+(defun add-cube (x y z)
+    (pzmq:with-socket socket :req
+      (pzmq:connect socket "tcp://localhost:5555")
+      (pzmq:send socket (cube-to-str x y z))))
